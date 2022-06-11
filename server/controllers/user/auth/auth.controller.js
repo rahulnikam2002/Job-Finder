7d5a2118f68e93022f3e8647958c9c5c72bf18bc
Rahul Nikam <codewithrahulnikam@gmail.com>
2022-06-11 17:56:43 +0530

    Completed login api

--- server/controllers/user/auth/auth.controller.js
+++ server/controllers/user/auth/auth.controller.js
@@ -2,6 +2,7 @@ const db = require("../../../db/db");
 const { genSaltSync, compareSync, hashSync, hash } = require("bcrypt");
 const { query } = require("express");
 const { use } = require("../../../routers/user/auth/auth.router");
+const { sign, TokenExpiredError } = require("jsonwebtoken")
 
 exports.userSignup = (req, res) => {
   const { userName, userEmail, userPassword } = req.body;
@@ -26,10 +27,58 @@ exports.userSignup = (req, res) => {
                     console.log("Err while inserting data to db")
                 }
                 else{
-                    res.send("User added to database")
+                    let userToken = sign({userEmail},"123456789", {
+                        expiresIn: "24h"
+                    })
+
+                    res.cookie("authToken", userToken, {
+                        expires: new Date(Date.now() + 86400000)
+                    })
+
+                    res.send({
+                        msg: "Registered success",
+                        code: 1,
+                        isLogin: true
+                    })
                 }
             })
         }
     }
   );
 };
+
+
+exports.userLogin = (req,res) => {
+    const {userEmail, userPassword} = req.body;
+
+    db.query("select * from users where userEmail = ?", [userEmail], (err,data) => {
+        if(err){
+            console.log("Error while fetching the data in userLogin");
+        }
+        else{
+            if(data.length > 0){
+                let password = data[0].userPassword;
+                password = compareSync(userPassword, password);
+                console.log(password)
+                if(password){
+                    let userToken = sign({userEmail},"123456789", {
+                        expiresIn: "24h"
+                    })
+
+                    res.cookie("authToken", userToken, {
+                        expires: new Date(Date.now() + 86400000)
+                    })
+
+                    res.send({
+                        msg: "Login success",
+                        code: 1,
+                        isLogin: true
+                    })
+                }
+            }
+            else{
+                res.send("No user found");
+            }
+        }
+    })
+}
--- server/routers/user/auth/auth.router.js
+++ server/routers/user/auth/auth.router.js
@@ -4,5 +4,6 @@ const controller = require("../../../controllers/user/auth/auth.controller.js");
 
 
 router.post("/signup", controller.userSignup)
+router.post("/login", controller.userLogin)
 
 module.exports = router
